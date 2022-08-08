7d7ac798b2b5b9fabe345a1ad1d30b9c5699801b
Skystra <97003835+Skystra69@users.noreply.github.com>
2022-08-08 08:20:22 +0100

    Add files via upload

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,8 +1,85 @@
 import random, string
-length = int(input("How long will your password be? "))
-char = string.ascii_letters + string.digits + string.punctuation
-password = []
-for x in range(length):
-    password.append(random.choice(char))
-print("Your password is " + ''.join(password))
-a=input("Press close to exit the program.")
+import os
+import time
+loopCode = True
+loopCode2 = True
+
+while loopCode == True:
+    try:
+        print('''Password Generator v1.1
+    ''')
+        length = int(input('''How many characters should be in your password?
+A length between 8-16 characters is recommended:
+'''))
+    except ValueError:
+        print("This is not a valid number, please try again.")
+        time.sleep(2)
+        os.system('cls')
+        continue
+    
+    while loopCode2 == True:
+        try:
+            print ("")
+            userDecision = int(input('''Please choose the complexity of the password.
+It is recommended to have a mix of character types in your password:
+
+1: Only lowercase
+2: Lowercase and uppercase
+3: Lowercase, uppercase, and numbers
+4: Lowercase, uppercase, numbers and symbols
+'''))
+        except ValueError:
+            print('''
+Something went wrong, please ensure you are entering a valid number.''')
+            time.sleep(2)
+            os.system('cls')
+            continue
+    
+        if userDecision == 1:
+            char = string.ascii_lowercase
+            password = []
+            for x in range(length):
+                password.append(random.choice(char))
+            print("")
+            print("Your generated password is " + ''.join(password))
+            break
+        
+        elif userDecision == 2:
+            char = string.ascii_lowercase + string.ascii_uppercase
+            password = []
+            for x in range(length):
+                password.append(random.choice(char))
+            print("")
+            print("Your generated password is " + ''.join(password))
+            break
+        
+        elif userDecision == 3:
+            char = string.ascii_lowercase + string.ascii_uppercase + string.digits
+            password = []
+            for x in range(length):
+                password.append(random.choice(char))
+            print("")
+            print("Your generated password is " + ''.join(password))
+            break
+        
+        elif userDecision == 4:
+            char = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+            password = []
+            for x in range(length):
+                password.append(random.choice(char))
+            print("")
+            print("Your generated password is " + ''.join(password))
+            break
+        
+        else:
+            print('''
+Something went wrong, ensure you are entering a valid number.''')
+            time.sleep(2)
+            os.system('cls')
+
+    print("")
+    input('''Press enter if you wish to generate another password.
+Alternatively, press close to exit the program.
+''')
+    os.system('cls')
+
