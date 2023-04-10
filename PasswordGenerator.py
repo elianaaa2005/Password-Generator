6fb77b709e8f73f8759ff9d2bc452b243ec0fec8
Enigma <97003835+ElianaEnigma@users.noreply.github.com>
2023-04-11 00:35:31 +0100

    Add files via upload

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,85 +1,50 @@
-import random, string
-import os
-import time
-loopCode = True
-loopCode2 = True
+import random
+import string
 
-while loopCode == True:
-    try:
-        print('''Password Generator v1.1
-    ''')
-        length = int(input('''How many characters should be in your password?
-A length between 8-16 characters is recommended:
-'''))
-    except ValueError:
-        print("This is not a valid number, please try again.")
-        time.sleep(2)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        continue
-    
-    while loopCode2 == True:
+def generate_password(length, complexity):
+    char_sets = {
+        1: string.ascii_lowercase,
+        2: string.ascii_letters,
+        3: string.ascii_letters + string.digits,
+        4: string.printable.strip(),
+    }
+    char_set = char_sets.get(complexity, string.ascii_lowercase)
+    password = ''.join(random.choice(char_set) for _ in range(length))
+    return password
+
+while True:
+    print("Password Generator v1.1")
+    while True:
         try:
-            print ("")
-            userDecision = int(input('''Please choose the complexity of the password.
-It is recommended to have a mix of character types in your password:
+            length = int(input("How many characters should be in your password? (8-16 characters): "))
+            if 8 <= length <= 16:
+                break
+            print("Please enter a valid number between 8 and 16.")
+        except ValueError:
+            print("Please enter a valid number between 8 and 16.")
 
+    while True:
+        try:
+            complexity = int(input('''Please choose the complexity of the password:
 1: Only lowercase
 2: Lowercase and uppercase
 3: Lowercase, uppercase, and numbers
 4: Lowercase, uppercase, numbers and symbols
 '''))
+            if 1 <= complexity <= 4:
+                break
+            print("Please enter a valid number between 1 and 4.")
         except ValueError:
-            print('''
-Something went wrong, please ensure you are entering a valid number.''')
-            time.sleep(2)
-            os.system('cls' if os.name == 'nt' else 'clear')
-            continue
-    
-        if userDecision == 1:
-            char = string.ascii_lowercase
-            password = []
-            for x in range(length):
-                password.append(random.choice(char))
-            print("")
-            print("Your generated password is " + ''.join(password))
+            print("Please enter a valid number between 1 and 4.")
+
+    password = generate_password(length, complexity)
+    print(f"Your generated password is: {password}")
+
+    while True:
+        choice = input("Press enter to generate another password, or type 'close' to exit: ")
+        if choice.lower() == 'close':
             break
-        
-        elif userDecision == 2:
-            char = string.ascii_lowercase + string.ascii_uppercase
-            password = []
-            for x in range(length):
-                password.append(random.choice(char))
-            print("")
-            print("Your generated password is " + ''.join(password))
-            break
-        
-        elif userDecision == 3:
-            char = string.ascii_lowercase + string.ascii_uppercase + string.digits
-            password = []
-            for x in range(length):
-                password.append(random.choice(char))
-            print("")
-            print("Your generated password is " + ''.join(password))
-            break
-        
-        elif userDecision == 4:
-            char = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-            password = []
-            for x in range(length):
-                password.append(random.choice(char))
-            print("")
-            print("Your generated password is " + ''.join(password))
-            break
-        
+        elif choice:
+            print("Please press enter or type 'close'.")
         else:
-            print('''
-Something went wrong, ensure you are entering a valid number.''')
-            time.sleep(2)
-            os.system('cls' if os.name == 'nt' else 'clear')
-
-    print("")
-    input('''Press enter if you wish to generate another password.
-Alternatively, press close to exit the program.
-''')
-    oos.system('cls' if os.name == 'nt' else 'clear')
-
+            break
